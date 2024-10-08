71ff58d6256bd7a2f099794dfb87868178f87feb
h.sb <jksg01019@gmail.com>
2024-10-08 18:47:18 +0900

    doing

--- test.py
+++ test.py
@@ -10,12 +10,12 @@ model.info()
 
 # Run inference
 start_time = time.time()
-model("examples/soccer/data/output.mp4", device="mps")
+model("examples/soccer/data/output.mp4", device="cuda")
 print(f"Finished in {time.time() - start_time:.2f} seconds")
 
 
-#
-# from ultralytics.data.annotator import auto_annotate
-#
-# auto_annotate(data="path/to/images", det_model="yolov8x.pt", sam_model="sam2_b.pt")
-#
+
+from ultralytics.data.annotator import auto_annotate
+
+auto_annotate(data="path/to/images", det_model="yolov8x.pt", sam_model="sam2_b.pt", device="cuda")
+
