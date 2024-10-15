d669494946271fd0fc2aae8de5d58557f1dadf9c
h.sb <jksg01019@gmail.com>
2024-10-15 23:49:26 +0900

    doing

--- sports/configs/futsal.py
+++ sports/configs/futsal.py
@@ -44,7 +44,7 @@ class FutsalPitchConfiguration:
 
     labels: List[str] = field(default_factory=lambda: [
         "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
-        "11", "12", "13", "15", "16", "17"
+        "11", "12", "13", "14", "15", "16", "17"
     ])
 
     colors: List[str] = field(default_factory=lambda: [
--- train_keypoint.py
+++ train_keypoint.py
@@ -1,4 +1,11 @@
 from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+import supervision as sv
+from typing import List
+from dataclasses import dataclass, field
+import cv2
+
 """
 yolov8n-pose.pt 
 yolov8s-pose.pt 
@@ -9,14 +16,62 @@ yolov8x-pose-p6.pt
 
 tensorboard --logdir runs/pose/train6
 
-"""
-model = YOLO('yolov8x-pose.pt')
-
-results = model.train(data='./test_dataset/data.yaml', epochs=125000, patience=125000000, imgsz=640, device='cuda:0', batch=4, mosaic=0.0, plots=True)
-
+이미지를 한장씩 
 
 """
-engine/trainer: task=pose, mode=train, model=yolov8x-pose.pt, data=./test_dataset/data.yaml, epochs=25000, time=None, patience=100, batch=4, imgsz=640, save=True, save_period=-1, cache=False, device=cuda:0, workers=8, project=None, name=train4, exist_ok=False, pretrained=True, optimizer=auto, verbose=True, seed=0, deterministic=True, single_cls=False, rect=False, cos_lr=False, close_mosaic=10, resume=False, amp=True, fraction=1.0, profile=False, freeze=None, multi_scale=False, overlap_mask=True, mask_ratio=4, dropout=0.0, val=True, split=val, save_json=False, save_hybrid=False, conf=None, iou=0.7, max_det=300, half=False, dnn=False, plots=True, source=None, vid_stride=1, stream_buffer=False, visualize=False, augment=False, agnostic_nms=False, classes=None, retina_masks=False, embed=None, show=False, save_frames=False, save_txt=False, save_conf=False, save_crop=False, show_labels=True, show_conf=True, show_boxes=True, line_width=None, format=torchscript, keras=False, optimize=False, int8=False, dynamic=False, simplify=True, opset=None, workspace=4, nms=False, lr0=0.01, lrf=0.01, momentum=0.937, weight_decay=0.0005, warmup_epochs=3.0, warmup_momentum=0.8, warmup_bias_lr=0.1, box=7.5, cls=0.5, dfl=1.5, pose=12.0, kobj=1.0, label_smoothing=0.0, nbs=64, hsv_h=0.015, hsv_s=0.7, hsv_v=0.4, degrees=0.0, translate=0.1, scale=0.5, shear=0.0, perspective=0.0, flipud=0.0, fliplr=0.5, bgr=0.0, mosaic=0.0, mixup=0.0, copy_paste=0.0, copy_paste_mode=flip, auto_augment=randaugment, erasing=0.4, crop_fraction=1.0, cfg=None, tracker=botsort.yaml, save_dir=/home/hsb/PycharmProjects/sports/runs/pose/train4
-Overriding model.yaml kpt_shape=[17, 3] with kpt_shape=[16, 3]
 
-"""
+
+TRAIN_MODE = False
+if TRAIN_MODE:
+    model = YOLO('yolov8x-pose.pt')
+    results = model.train(data='./test_dataset/data.yaml', epochs=125000, patience=125000000, imgsz=640, device='cuda:0', batch=4, mosaic=0.0, plots=True,)
+else:
+    """
+앞의 13개는 "#FF1493"로 **분홍색(D eep Pink)**이고, 
+뒤의 4개는 "#00BFFF"로 **파란색(Deep Sky Blue)**이야.
+    """
+    colors: List[str] = [
+        "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493",
+        "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493", "#FF1493",
+        "#FF1493", "#00BFFF", "#00BFFF", "#00BFFF"
+    ]
+
+    VERTEX_LABEL_ANNOTATOR = sv.VertexLabelAnnotator(
+        color=[sv.Color.from_hex(color) for color in colors],
+        text_color=sv.Color.from_hex('#FFFFFF'), # white
+        border_radius=5,
+        text_thickness=1,
+        text_scale=0.5,
+        text_padding=5,
+    )
+    labels: List[str] = [
+        "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+        "11", "12", "13", "14", "15", "16"
+    ]
+
+    # 모델 로드 (학습한 가중치가 저장된 모델 경로를 사용)
+    model = YOLO('runs/pose/train7/weights/best.pt')  # 학습이 끝난 후의 모델 가중치 파일 경로
+
+    # 테스트할 데이터셋의 경로 (학습 시 사용한 test 데이터 경로)
+    test_data = './test_dataset/train/images'  # test 이미지가 있는 경로로 수정
+
+    # 추론 실행
+    results = model.predict(source=test_data, save=True, save_txt=True,
+                            imgsz=640, conf=0.1,
+                            )
+    for a_result in results:
+        keypoints = sv.KeyPoints.from_ultralytics(a_result)
+        print("keypoints.xy.shape:", keypoints.xy.shape) # (3, 16, 2)
+        annotated_frame = a_result.orig_img
+        annotated_frame = VERTEX_LABEL_ANNOTATOR.annotate(
+            annotated_frame, keypoints, labels)
+
+        cv2.imshow("frame", annotated_frame)
+
+        # Enter 키를 누르면 다음 이미지로 넘어감 (13은 Enter의 ASCII 코드)
+        while True:
+            if cv2.waitKey(1) == 13:  # Enter 키를 누를 때까지 대기
+                break
+
+    # 모든 창 닫기
+    cv2.destroyAllWindows()
